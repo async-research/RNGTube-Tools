16c116a1df770d1cfc7c1c430326147355cd0169
fresh-patches <ggrodriguez.dev@gmail.com>
2021-07-17 23:56:58 -0700

    added progress bars and switched to requests instead of wget

--- background_gen.py
+++ background_gen.py
@@ -2,51 +2,60 @@
 """
     Creates a collage of thumbnails with top rated videos from RNGTube Database.
 """
-import os, glob, time, wget, mysql.connector
+import os, glob, time, mysql.connector, requests
 from PIL import Image, ImageFilter
 from dotenv import load_dotenv
 from os import getenv
+from tqdm import tqdm
 
 PADDING = 100 
 
+
+def download_file(url,addendum="", path="", pb=False):
+    file_name = url.split("/")[-1]
+    if addendum != "":
+        file_name = file_name + "_" + addendum
+    response = requests.get(url, stream=True)
+    with open(f"{path}{file_name}","wb") as f:
+        with tqdm(
+            unit="B", unit_scale=True, desc=file_name, ncols=PADDING, disable=pb, 
+            total=int(response.headers.get("content-length",0))
+            ) as pbar:
+                for chunk in response.iter_content(chunk_size=1024):
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+
 def download_thumbnails(conn,dir_path,table_name='videos'):
-    image_files = glob.glob(dir_path + "/images/*.jpg")
     width=320
     height=180
-    query = "SELECT videoID FROM  videos ORDER BY likeCount DESC LIMIT 300"
+    query = "SELECT videoID FROM  videos ORDER BY likeCount DESC LIMIT 256"
     cur = conn.cursor()
     cur.execute(query);
-
     videoIDs = [videoID[0] for videoID in cur.fetchall()]
-    cnt = 0
-    for videoID in videoIDs:
-        if cnt >= 100:
-            break
+   
+    for i in tqdm(range(len(videoIDs)), ncols=PADDING):
         try:
-            img_filename = wget.download("http://i.ytimg.com/vi/" + videoID + "/mqdefault.jpg", dir_path + "/images/" + videoID + ".jpg")
-
+            url = f"http://i.ytimg.com/vi/{videoIDs[i]}/mqdefault.jpg"
+            download_file(url, videoIDs[i], path=dir_path+"/images/", pb=True)
+            
         except Exception as e:
-            print("ERROR: IMAGE NOT DOWNLOADED, VideoID:", videoID)
-            print(e)
+            print(f"\033[91m ERROR!: {videoIDs[i]}: {e}\033[00m".center(80," "))
+          
         else:
             time.sleep(.2)
-            cnt += 1 
+      
+    
 
 def create_background(dir_path, save_dir):
     image_files = glob.glob(dir_path + "/images/*.jpg")
-    cnt=0
-    for img_file in image_files:
-        cnt += 1
     c_width = 320*5
     c_height=180*5
     image_files.sort(key=os.path.getmtime)
     main = Image.new("RGB",(c_width,c_height))
     width = 0
     height = 0
-    cnt=0
-
+ 
     for img_file in image_files:
-        cnt += 1
         img = Image.open(img_file)
         smll_img = img.resize((round(img.size[0]*0.5), round(img.size[1]*0.5)))
         smll_img = Image.eval(smll_img, lambda x: x/2) #pixle calcs
@@ -57,18 +66,18 @@ def create_background(dir_path, save_dir):
             height += round(img.size[1]*0.5)
         if height >= c_height:
             break
-    assert cnt == 100
+
     main.save(save_dir + '/toplikes.jpg')
     print()
-    print('Saved collage to ' + save_dir + '/toplikes.jpg')
+    print(('Saved collage to ' + save_dir + '/toplikes.jpg').center(PADDING, " "))
 
     for img_file in image_files:
         os.remove(img_file)
-    print(dir_path + ' cleaned!')
+    print((dir_path + ' cleaned!').center(PADDING, " "))
 
 
 def main():
-    print("Generating-RNGTUBE.COM-BACKGROUND-IMAGE".center(100, "@"))
+    print("Generating-RNGTUBE.COM-BACKGROUND-IMAGE".center(PADDING, "@"))
     load_dotenv()
     dir_path = os.getcwd()
     save_dir = '/var/www/html/rngtube.com/images'
